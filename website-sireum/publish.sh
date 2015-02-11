19059b3dc19a6fc193e6a4fb0660ab7d567ed797
Robby <robby@k-state.edu>
2015-02-11 14:36:35 -0600

    Updated to use GitHub Pages.

--- website-sireum/publish.sh
+++ website-sireum/publish.sh
@@ -1,4 +1,15 @@
-cd src/main/rst
-touch source/index.rst; make html
-scp -r build/html/* santos_jenkins@cislinux.cis.ksu.edu:/research/santos/web/sireum.org/
-cd ../../..
+rm -fR target
+mkdir target
+cd target
+git clone git@github.com:sireum/gh-pages-sireum.git
+rm -fR gh-pages-sireum/*
+cd ../src/main/rst
+make clean
+touch source/index.rst
+make html
+cp -R build/html/* ../../../target/gh-pages-sireum/
+cd ../../../target/gh-pages-sireum
+echo "sireum.org" > CNAME
+git add --all
+git commit -m 'Updated.'
+git push
