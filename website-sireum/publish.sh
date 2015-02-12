8ccc2889d5601a66f1c9b5bd6d6efb3ad758da3c
Robby <robby@k-state.edu>
2015-02-11 22:08:54 -0600

    Redirects sireum.org/download to sireum.org/software.html.

--- website-sireum/publish.sh
+++ website-sireum/publish.sh
@@ -10,6 +10,8 @@ make html
 cp -R build/html/* ../../../target/gh-pages-sireum/
 cd ../../../target/gh-pages-sireum
 echo "sireum.org" > CNAME
+mkdir download
+echo $'<html><head><meta http-equiv="Refresh" content="0; url=http://sireum.org/software.html"/></head><body><p>Please follow <a href="http://sireum.org/software.html">this link</a>.</p></body></html>' > download/index.html
 git add --all
 git commit -m 'Updated.'
 git push
